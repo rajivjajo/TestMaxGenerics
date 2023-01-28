bb19ca231feec8494f34d12b8f419520cc6ec47a
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-28 19:44:36 +0530

    Refactor:Maximum number is at 1st,2nd,3rd position for three integer

--- src/com/bridgelabz/ManNumber.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class ManNumber {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Test max Number using Generics!");
-    }
-}
