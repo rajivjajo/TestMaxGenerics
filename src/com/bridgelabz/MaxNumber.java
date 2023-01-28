9b057b8c7c898bb46fbd53cddb90ecb6402f7526
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-29 02:10:54 +0530

    Refactor:Maximum number is at 1st,2nd,3rd position for three floats values

--- src/com/bridgelabz/MaxNumber.java
+++ src/com/bridgelabz/MaxNumber.java
@@ -1,5 +1,5 @@
 package com.bridgelabz;
-public class MaxNumber <T extends Comparable<T>>{
+public class MaxNumber <T extends Comparable>{
     public T findMax(T num1, T num2, T num3) {
         T max = num1;
 
@@ -14,9 +14,9 @@ public class MaxNumber <T extends Comparable<T>>{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Test max Number using Generics!");
-        MaxNumber max= new MaxNumber<>();
-        System.out.println("Maximum number: " + max.findMax(40, 30, 10));
-        System.out.println("Maximum number: " + max.findMax(40, 20, 50));
-        System.out.println("Maximum number: " + max.findMax(50, 40, 30));
+        MaxNumber<Float> max= new MaxNumber<>();
+        System.out.println("Maximum number: " + max.findMax(5.0f, 3.0f, 1.0f));
+        System.out.println("Maximum number: " + max.findMax(4.0f, 20f, 5.0f));
+        System.out.println("Maximum number: " + max.findMax(6.f, 4.0f, 30.f));
     }
 }
