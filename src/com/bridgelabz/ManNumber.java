658e6f47c3ba655ecd56e7c7c745d91cddf90df8
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-28 19:30:48 +0530

    Welcome to the Max number problem using generics!

---
+++ src/com/bridgelabz/ManNumber.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class ManNumber {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Test max Number using Generics!");
+    }
+}
