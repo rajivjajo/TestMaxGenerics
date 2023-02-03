b45e18077777340cddca0c5b158e9931ae87e256
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-03 17:52:52 +0530

    Refactoring to create Generic Class to take in 3 variables of Generic Type.

--- src/com/bridgelabz/MaximumFinder.java
+++ src/com/bridgelabz/MaximumFinder.java
@@ -1,5 +1,19 @@
 package com.bridgelabz;
-public class MaximumFinder<T extends Comparable>{
+public class MaximumFinder<T extends Comparable<T>> {
+    private T num1;
+    private T num2;
+    private T num3;
+
+    public MaximumFinder(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public T testMaximum() {
+        return findMaximum(num1, num2, num3);
+    }
+
     public static <T extends Comparable<T>> T findMaximum(T num1, T num2, T num3) {
         T max = num1;
         max = max.compareTo(num2) > 0 ? max : num2;
@@ -8,8 +22,12 @@ public class MaximumFinder<T extends Comparable>{
     }
 
     public static void main(String[] args) {
-        System.out.println("Max int: " + MaximumFinder.findMaximum(5, 10, 3));
-        System.out.println("Max float: " + MaximumFinder.findMaximum(5.5f, 10.2f, 3.8f));
-        System.out.println("Max string: " + MaximumFinder.findMaximum("Apple", "Peach", "Banana"));
+        MaximumFinder<Integer> intFinder = new MaximumFinder<>(5, 10, 3);
+        System.out.println("Max int: " + intFinder.testMaximum());
+        MaximumFinder<Float> floatFinder = new MaximumFinder<>(5.5f, 10.2f, 3.8f);
+        System.out.println("Max float: " + floatFinder.testMaximum());
+        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana");
+        System.out.println("Max string: " + stringFinder.testMaximum());
     }
 }
+
