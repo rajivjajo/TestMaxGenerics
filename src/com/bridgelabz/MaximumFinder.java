e4dea3d97ed0a719fcac39b9a1c14782a639cbf5
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-03 18:05:53 +0530

    Using Options and using Sorting

--- src/com/bridgelabz/MaximumFinder.java
+++ src/com/bridgelabz/MaximumFinder.java
@@ -1,33 +1,30 @@
 package com.bridgelabz;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MaximumFinder<T extends Comparable<T>> {
-    private T num1;
-    private T num2;
-    private T num3;
+    private List<T> numbers;
 
-    public MaximumFinder(T num1, T num2, T num3) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
+    public MaximumFinder(T... numbers) {
+        this.numbers = Arrays.asList(numbers);
     }
-
     public T testMaximum() {
-        return findMaximum(num1, num2, num3);
+        return findMaximum(numbers);
     }
-
-    public static <T extends Comparable<T>> T findMaximum(T num1, T num2, T num3) {
-        T max = num1;
-        max = max.compareTo(num2) > 0 ? max : num2;
-        max = max.compareTo(num3) > 0 ? max : num3;
-        return max;
+    public static <T extends Comparable<T>> T findMaximum(List<T> numbers) {
+       Collections.sort(numbers);
+        return numbers.get(numbers.size() - 1);
     }
-
     public static void main(String[] args) {
-        MaximumFinder<Integer> intFinder = new MaximumFinder<>(5, 10, 3);
+        MaximumFinder<Integer> intFinder = new MaximumFinder<>(5, 10, 3, 15, 20);
         System.out.println("Max int: " + intFinder.testMaximum());
-        MaximumFinder<Float> floatFinder = new MaximumFinder<>(5.5f, 10.2f, 3.8f);
+        MaximumFinder<Float> floatFinder = new MaximumFinder<>(5.5f, 10.2f, 3.8f, 2.0f, 8.9f);
         System.out.println("Max float: " + floatFinder.testMaximum());
-        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana");
+        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana", "Cherry", "Grapes");
         System.out.println("Max string: " + stringFinder.testMaximum());
     }
+
 }
 
