cac9c0d1f41b2a9e1972e0ba8071bb0ff460f6ea
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-03 18:14:16 +0530

    Extending the max method to also print the max to std out using Generic Method.

--- src/com/bridgelabz/MaximumFinder.java
+++ src/com/bridgelabz/MaximumFinder.java
@@ -10,21 +10,27 @@ public class MaximumFinder<T extends Comparable<T>> {
     public MaximumFinder(T... numbers) {
         this.numbers = Arrays.asList(numbers);
     }
-    public T testMaximum() {
-        return findMaximum(numbers);
+
+    public void testMaximum() {
+        T max = findMaximum(numbers);
+        printMax(max);
     }
+
     public static <T extends Comparable<T>> T findMaximum(List<T> numbers) {
-       Collections.sort(numbers);
+        Collections.sort(numbers);
         return numbers.get(numbers.size() - 1);
     }
-    public static void main(String[] args) {
-        MaximumFinder<Integer> intFinder = new MaximumFinder<>(5, 10, 3, 15, 20);
-        System.out.println("Max int: " + intFinder.testMaximum());
-        MaximumFinder<Float> floatFinder = new MaximumFinder<>(5.5f, 10.2f, 3.8f, 2.0f, 8.9f);
-        System.out.println("Max float: " + floatFinder.testMaximum());
-        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana", "Cherry", "Grapes");
-        System.out.println("Max string: " + stringFinder.testMaximum());
+
+    public static <T> void printMax(T max) {
+        System.out.println("Max: " + max);
     }
 
+    public static void main(String[] args) {
+        MaximumFinder<Integer> intFinder = new MaximumFinder<>(5, 10, 3, 15, 20);
+        intFinder.testMaximum();
+        MaximumFinder<Float> floatFinder = new MaximumFinder<>(5.5f, 10.2f, 3.8f, 2.0f, 8.9f);
+        floatFinder.testMaximum();
+        MaximumFinder<String> stringFinder = new MaximumFinder<>("Apple", "Peach", "Banana", "Cherry", "Grapes");
+        stringFinder.testMaximum();
+    }
 }
-
